682772892bb5bcdddc9c1d62325916b942b5427f
David Wu <30442209+DW@users.noreply.github.com>
2018-10-18 19:10:16 -0500

    10/18/2018

---
+++ lib/main_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class MainPage extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MainPageState();
+  }
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Equation Solver"),
+      ),
+      body: Center(
+        child: Text("jj"),
+      ),
+    );
+  }
+}
