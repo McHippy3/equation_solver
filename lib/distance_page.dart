00073996992836adb54c37c4b5836dc2bf79f125
David Wu <30442209+DW@users.noreply.github.com>
2018-10-24 18:51:59 -0500

    10/24/2018

--- lib/distance_page.dart
+++ lib/distance_page.dart
@@ -8,54 +8,91 @@ class DistancePage extends StatefulWidget {
 }
 
 class _DistancePageState extends State<DistancePage> {
+  static TextEditingController x1controller = TextEditingController();
+  static TextEditingController y1controller = TextEditingController();
+  static TextEditingController x2controller = TextEditingController();
+  static TextEditingController y2controller = TextEditingController();
+
+  String answer = '';
+
   RowTextFields _x1 = RowTextFields(
-      frontText: 'X1 =',
-      widthText: 100.0,
-      widthTextField: 250.0,
-      fontSize: 24.0);
+    frontText: 'X1 =',
+    widthText: 100.0,
+    widthTextField: 250.0,
+    fontSize: 24.0,
+    controller: x1controller,
+  );
 
   RowTextFields _y1 = RowTextFields(
-      frontText: 'Y1 =',
-      widthText: 100.0,
-      widthTextField: 250.0,
-      fontSize: 24.0);
+    frontText: 'Y1 =',
+    widthText: 100.0,
+    widthTextField: 250.0,
+    fontSize: 24.0,
+    controller: y1controller,
+  );
 
   RowTextFields _x2 = RowTextFields(
-      frontText: 'X2 =',
-      widthText: 100.0,
-      widthTextField: 250.0,
-      fontSize: 24.0);
+    frontText: 'X2 =',
+    widthText: 100.0,
+    widthTextField: 250.0,
+    fontSize: 24.0,
+    controller: x2controller,
+  );
 
   RowTextFields _y2 = RowTextFields(
-      frontText: 'Y2 =',
-      widthText: 100.0,
-      widthTextField: 250.0,
-      fontSize: 24.0);
+    frontText: 'Y2 =',
+    widthText: 100.0,
+    widthTextField: 250.0,
+    fontSize: 24.0,
+    controller: y2controller,
+  );
 
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Distance Formula'),
       ),
-      body: ListView(children: <Widget>[
-        _x1,
-        _y1,
-        _x2,
-        _y2,
-        Padding(
-          padding: const EdgeInsets.only(top: 50.0),
-          child: Center(
-            child: RaisedButton(
-              child: Text('Calculate'),
-              onPressed: calculate,
+      body: ListView(
+        children: <Widget>[
+          _x1,
+          _y1,
+          _x2,
+          _y2,
+          Padding(
+            padding: const EdgeInsets.only(top: 50.0),
+            child: Center(
+              child: RaisedButton(
+                child: Text('Calculate'),
+                onPressed: calculate,
+              ),
             ),
           ),
-        ),
-      ]),
+          Padding(
+            padding: const EdgeInsets.all(50.0),
+            child: Center(
+              child: Text(answer, style: TextStyle(fontSize: 32.0)),
+            ),
+          ),
+        ],
+      ),
     );
   }
 
   void calculate() {
-    _x1.getValues();
+    try{
+
+    }
+    catch(Exception){
+      print('Error');
+    }
+    setState(() {});
+  }
+
+  @override
+  void dispose(){
+    x1controller.dispose();
+    y1controller.dispose();
+    x2controller.dispose();
+    y2controller.dispose();
   }
 }
--- lib/row_textfield.dart
+++ lib/row_textfield.dart
@@ -1,40 +1,43 @@
 import 'package:flutter/material.dart';
+import './distance_page.dart';
 
-class RowTextFields extends StatelessWidget{
-  String frontText;
+class RowTextFields extends StatelessWidget {
+  String frontText, currentText;
   double widthText, widthTextField, fontSize;
-  static TextEditingController controller = TextEditingController();
-  TextField tf = TextField(controller: controller,);
+  TextEditingController controller;
+  TextField tf;
 
-  RowTextFields({this.frontText = '', this.widthText = 0.0, this.widthTextField = 0.0, this.fontSize = 24.0} );
+  RowTextFields(
+      {this.frontText = '',
+      this.widthText = 0.0,
+      this.widthTextField = 0.0,
+      this.fontSize = 24.0,
+      this.controller});
 
   @override
-  Widget build (BuildContext context){
+  Widget build(BuildContext context) {
+    tf = TextField(controller: controller);
     return Row(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(15.0),
-              width: widthText,
-              child: Center(
-                child: Text(
-                  frontText,
-                  style: TextStyle(
-                    fontSize: fontSize,
-                  ),
-                ),
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: <Widget>[
+        Container(
+          padding: const EdgeInsets.all(15.0),
+          width: widthText,
+          child: Center(
+            child: Text(
+              frontText,
+              style: TextStyle(
+                fontSize: fontSize,
               ),
             ),
-            Container(
-              padding: const EdgeInsets.symmetric(vertical: 15.0),
-              width: widthTextField,
-              child: tf,
-            ),
-          ],
+          ),
+        ),
+        Container(
+          padding: const EdgeInsets.symmetric(vertical: 15.0),
+          width: widthTextField,
+          child: tf,
+        ),
+      ],
     );
-  }
-
-  String getValues(){
-    print(controller.text);
   }
 }
