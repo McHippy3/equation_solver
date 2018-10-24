6b71640791e93a1d676b10f7ef4976d59aabe347
KHSCS141 <KHSCS141@KATYISD.ORG>
2018-10-24 10:32:39 -0500

    10/24/2018

--- lib/distance_page.dart
+++ lib/distance_page.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'main_page.dart';
+import './row_textfield.dart';
 
 class DistancePage extends StatefulWidget {
   State<StatefulWidget> createState() {
@@ -8,30 +8,54 @@ class DistancePage extends StatefulWidget {
 }
 
 class _DistancePageState extends State<DistancePage> {
+  RowTextFields _x1 = RowTextFields(
+      frontText: 'X1 =',
+      widthText: 100.0,
+      widthTextField: 250.0,
+      fontSize: 24.0);
+
+  RowTextFields _y1 = RowTextFields(
+      frontText: 'Y1 =',
+      widthText: 100.0,
+      widthTextField: 250.0,
+      fontSize: 24.0);
+
+  RowTextFields _x2 = RowTextFields(
+      frontText: 'X2 =',
+      widthText: 100.0,
+      widthTextField: 250.0,
+      fontSize: 24.0);
+
+  RowTextFields _y2 = RowTextFields(
+      frontText: 'Y2 =',
+      widthText: 100.0,
+      widthTextField: 250.0,
+      fontSize: 24.0);
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Distance Formula'),
       ),
-      body: Column(
-        children: <Widget>[
-          Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Container(
-                padding: const EdgeInsets.symmetric(horizontal: 15.0),
-                width: 75.0,
-                child: Center(child: Text('Text')),
-              ),
-              Container(
-                padding: const EdgeInsets.symmetric(vertical: 25.0),
-                width: 200.0,
-                child: TextField(),
-              ),
-            ],
+      body: ListView(children: <Widget>[
+        _x1,
+        _y1,
+        _x2,
+        _y2,
+        Padding(
+          padding: const EdgeInsets.only(top: 50.0),
+          child: Center(
+            child: RaisedButton(
+              child: Text('Calculate'),
+              onPressed: calculate,
+            ),
           ),
-        ],
-      ),
+        ),
+      ]),
     );
+  }
+
+  void calculate() {
+    _x1.getValues();
   }
 }
